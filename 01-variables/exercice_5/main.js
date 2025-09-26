fdd9095cf2c1b2c7073d6388a17e2528f9837331
Obeis-Ndombe <obeissancendombe@gmail.com>
2025-09-25 19:41:05 -0700

    conversion de la température de Celsius en Fahrenheit

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,2 +1,11 @@
 // Exercice 5
 // Écrivez votre code ici
+
+// Stocker une température en Celsius
+let celsius = 25;
+
+// Conversion en Fahrenheit
+let fahrenheit = (celsius * 9/5) + 32;
+
+// Affichage du résultat
+console.log(celsius + "°C = " + fahrenheit + "°F");
